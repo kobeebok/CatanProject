b23bdb46d6f1a47af3215ae5e1c8d1b94d7318ed
kobeebok <65086998+kobeebok@users.noreply.github.com>
2023-04-28 01:44:21 +0100

    Update UIManager.cs

--- GameMechanics-Scripts/UIManager.cs
+++ GameMechanics-Scripts/UIManager.cs
@@ -6,11 +6,11 @@ public class UIManager : MonoBehaviour
 {
     public void ToggleBuildMenu()
     {
-        // Implement build menu display logic
+      
     }
 
     public void ToggleTradeMenu()
     {
-        // Implement trade menu display logic
+      
     }
 }
