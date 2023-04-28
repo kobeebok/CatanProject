5a31460f36520c24c50aa13c576392a4bcd954cd
kobeebok <65086998+kobeebok@users.noreply.github.com>
2023-04-28 01:48:30 +0100

    Update AIPlayerController.cs

--- AIPlayerController.cs
+++ AIPlayerController.cs
@@ -22,43 +22,35 @@ public class AIPlayerController : PlayerController
 
     private IEnumerator AIMakeDecision()
     {
-        // Wait for a short delay before making a decision
+        
         yield return new WaitForSeconds(decisionDelay);
 
-        // Add decision-making logic here
-        // Example: If the AI player has enough resources, build a settlement or a city
+      
         if (CanBuildSettlement())
         {
-            gameManager.BuildSettlement(playerID);
+            
         }
         else if (CanBuildCity())
         {
-            gameManager.BuildCity(playerID);
+            
         }
         else if (CanBuildRoad())
         {
-            gameManager.BuildRoad(playerID);
+            
         }
 
-        // End the AI player's turn
-        gameManager.EndTurn();
+        
     }
 
     private bool CanBuildSettlement()
     {
-        // Check if the AI player has the required resources to build a settlement
-        // Return true if possible, false otherwise
-    }
+        }
 
     private bool CanBuildCity()
     {
-        // Check if the AI player has the required resources to build a city
-        // Return true if possible, false otherwise
     }
 
     private bool CanBuildRoad()
     {
-        // Check if the AI player has the required resources to build a road
-        // Return true if possible, false otherwise
     }
 }
